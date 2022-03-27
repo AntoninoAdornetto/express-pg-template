65e887efaa7c38d487b6167b0ed4578615007635
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-03-27 18:22:15 -0400

    [EDIT] moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,10 @@ module.exports = {
   },
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1',
+    '@api/(.*)': '<rootDir>/src/api/$1',
+    '@config/(.*)': '<rootDir>/src/config/$1',
+    '@services/(.*)': '<rootDir>/src/services/$1',
+    '@loaders/(.*)': '<rootDir>/src/loaders/$1',
     '@tests/(.*)': '<rootDir>/tests/$1',
   },
   moduleDirectories: ['node_modules', 'src'],
