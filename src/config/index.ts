179c0aa72a5a1835683fb8008b9576b1b36f21f8
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-03-27 16:51:27 -0400

    [ADD] app configs / env vars

---
+++ src/config/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const env = dotenv.config();
+
+if (env.error) {
+  throw new Error('Could not locate .env file');
+}
+
+export default {
+  port: process.env.PORT,
+  dataBaseUser: process.env.DB_USER,
+  dataBasePass: process.env.DB_PASS,
+  dataBase: process.env.DB,
+  dataBaseHost: process.env.DB_HOST,
+  dataBasePort: parseInt(process.env.DB_PORT as string),
+  logs: {
+    level: process.env.LOG_LEVEL || 'silly',
+  },
+};
