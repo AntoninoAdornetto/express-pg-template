ea82ac53eaa6130f115611b818b4c3e379bd4fe5
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-04-03 00:58:33 -0400

    [ADD] trying a different approach

---
+++ src/api/routes/health/index.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response } from 'express';
+const route = Router();
+
+export default (app: Router) => {
+  app.use('/status', route);
+
+  route.get('/', (req: Request, res: Response) => {
+    res.status(200).send({
+      message: 'nice and healthy',
+    });
+  });
+
+  route.post('/', (req: Request, res: Response) => {
+    const { status } = req.body;
+
+    if (!status) return res.status(400).send({ errorMessage: 'Need to send a status' });
+
+    res.status(201).send({
+      message: `My current status is: ${status}`,
+    });
+  });
+};
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+import routes from '@api/index';
+import config from '@src/config';
 import errorMiddleware from '@api/middlewares/error';
 import ErrorHandler from '@services/errorHandler';
 
@@ -8,6 +10,8 @@ export default ({ app }: { app: express.Application }) => {
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
   }
+
+  app.use(config.api.prefix, routes());
 
   app.all('*', (req: Request, res: Response, next: NextFunction) => {
     next(new ErrorHandler(`No route found for ${req.originalUrl}`, 404));
