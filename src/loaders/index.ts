d8c3c66d65fc67238ce950891f8a872dc66fddd0
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-04-03 01:40:59 -0400

    [FIX] resolved server start up bug

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,9 +1,8 @@
-import express from 'express';
 import expressLoader from './express';
 import pool from './postgres';
 import logger from './logger';
 
-export default async (expressApp: express.Application) => {
+export default async ({ expressApp }: any) => {
   await pool.connect();
   logger.info('✌️ DB loaded and connected!');
 
